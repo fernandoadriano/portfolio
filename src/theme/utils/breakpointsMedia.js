4ea9c569c71b598b1591a3f1ba9e59c98cc8dca9
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-06 22:44:39 -0300

    fix: corrigido risco potencial em tratamento de arrays

--- src/theme/utils/breakpointsMedia.js
+++ src/theme/utils/breakpointsMedia.js
@@ -6,8 +6,8 @@ export default function breakpointsMedia(cssByBreakpoint) {
   return breakpointNames
     .filter((breakpointName) => Boolean(cssByBreakpoint[String(breakpointName)]))
     .map((breakpointName) => css`
-            @media only screen and (min-width: ${breakpoints[breakpointName]}px) {
-            ${cssByBreakpoint[breakpointName]}
+            @media only screen and (min-width: ${breakpoints[String(breakpointName)]}px) {
+            ${cssByBreakpoint[String(breakpointName)]}
         }
         `);
 }
