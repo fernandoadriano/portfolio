e79c87f77fecd4db1cf507d27ebcf2f26d95f76c
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-21 12:24:39 -0300

    fix(fonts): ajustado a importação do google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Portifólio - Projeto do Bootcamp JAM Stack Alura</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" /> */}
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
+        </style>
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
