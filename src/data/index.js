3232eb6f447bc1a4880cc794c9431770781b9c87
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-04-24 15:00:53 -0300

    feat: alterado para que a lista de projetos venham do DATO CMS

--- src/data/index.js
+++ src/data/index.js
@@ -1,21 +1,36 @@
-import { projects } from './db.json';
+import { CMSGraphQLClient, gql } from 'src/infra/cms/DATOClient';
+// import import { projects } from './db.json';
 
 export default {
   projectList: async () => {
-    const resposta = await fetch('https://api.github.com/users/fernandoadriano/repos');
-
-    if (resposta.status !== 200) {
-      throw new Error(`Erro ao acessar github: ${resposta.status} - ${resposta.statusText}`);
+    // const resposta = await fetch('https://api.github.com/users/fernandoadriano/repos');
+    const client = CMSGraphQLClient();
+    const query = gql`query 
+    {
+      allProjetos {
+         id
+        nome
+        slug
+        descricao(markdown: false)
+        url
+        repositorio
+        screenshot {
+          url
+        }
+      }
     }
+    `;
+    const resposta = await client.query({ query });
+    console.log(JSON.stringify(resposta, null, 4));
 
-    const gitProjects = await resposta.json();
-    const projetos = gitProjects.map((projeto) => ({
+    const projetos = resposta.data.allProjetos.map((projeto) => ({
       id: projeto.id,
-      nome: projeto.name.replace(/[_-]/gm, ' '),
-      descricao: projeto.description,
-      url: projeto.html_url,
-      slug: projeto.name,
-      screenshot: `/images/${projeto.name}.png`,
+      nome: projeto.nome,
+      descricao: projeto.descricao,
+      url: projeto.url,
+      repositorio: projeto.repositorio,
+      slug: projeto.slug,
+      screenshot: projeto.screenshot.url,
     }));
 
     return projetos;
