878f358db15103b1c2ff833f3e925f475804620b
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-04-24 19:36:04 -0300

    fix: corrigido problemas eslint

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
 
